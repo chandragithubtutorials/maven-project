af2fd4f4ce7e4eb6febce4571c8d88ba3ed3766d
chandragithubtutorials <160589181+chandragithubtutorials@users.noreply.github.com>
2024-03-14 16:29:21 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,7 +11,7 @@ public class Greeter {
 	public Greeter() {
 			 }	    
 	/**
-	 * ** @param someone the name of a person
+	 * ** @chandra some one person
 	 * ** @return greeting string
 	 * **/
 	public final String greet(final String someone) {
